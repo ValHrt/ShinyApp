4ad24f86b0793b1d4431f209105718b4f72969bd
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2020-11-22 18:24:06 +0100

    Premier module complet stable

--- CorporateFi/app.R
+++ CorporateFi/app.R
@@ -11,7 +11,6 @@
 library(shiny)
 library(ggplot2)
 library(tidyverse)
-library(readr)
 
 #Define UI ----
 ui <- fluidPage(
@@ -32,20 +31,42 @@ ui <- fluidPage(
                          label = "Taux (ex : si 1% entrer 1 et non 0,01) :",
                          value = 0)
         ),
-        mainPanel(h3(textOutput("CoutCredit"))
+        mainPanel(
+            h3(textOutput("CoutAnnuite"),
+               br(),
+                  h3(textOutput("CoutCredit"),
+                     br(),
+                     h3(textOutput("CoutInterets"))
             
         )
     )
-)
+)))
 
 # Define server logic ----
 server <- function(input, output, session) {
+    output$CoutAnnuite <- renderText({
+firstF <- (input$Tx/100) / (1-(1+input$Tx/100)^-input$D)
+secondF <- input$C
+results <- firstF * secondF
+paste("Le coût de l'annuité constante s'élève à ", results)
+    })
+    
     output$CoutCredit <- renderText({
 firstF <- (input$Tx/100) / (1-(1+input$Tx/100)^-input$D)
 secondF <- input$C
 results <- firstF * secondF
-paste("Le coût de l'annuité constante s'élève à ", results)  
-})
+thirdF <- results * input$D
+paste("Le coût total du crédit s'élève à ", thirdF)
+    })
+    
+    output$CoutInterets <- renderText({
+firstF <- (input$Tx/100) / (1-(1+input$Tx/100)^-input$D)
+secondF <- input$C
+results <- firstF * secondF
+thirdF <- results * input$D
+fourthF <- thirdF - input$C
+paste("Le coût total des intérêts s'élèvent à ", fourthF)
+    })
 }
 
 # Run the App ----
