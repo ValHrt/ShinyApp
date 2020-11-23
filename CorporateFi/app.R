06dfc461c15d5066210805828eb3c0a40d877f39
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2020-11-23 15:11:08 +0100

    Rajout IRR / NPV

--- CorporateFi/app.R
+++ CorporateFi/app.R
@@ -11,11 +11,12 @@ library(shiny)
 library(shinythemes)
 library(ggplot2)
 library(tidyverse)
+library(FinancialMath)
 
 #Define UI ----
 ui = fluidPage(theme = shinytheme("superhero"),
     navbarPage("Corporate Finance",
-    tabPanel("Crédit",
+    tabPanel("Crédit",{
     sidebarLayout(
         sidebarPanel(
             h2("Calcul du coût de l'annuité constante"),
@@ -41,9 +42,10 @@ ui = fluidPage(theme = shinytheme("superhero"),
                   )
             )
         )
-    )
+    )}
     ),
-    tabPanel("Perpetuity",
+    
+    tabPanel("Perpetuity",{
              sidebarLayout(
                  sidebarPanel(
                      h2("Calcul de la valeur de la perpétuité"),
@@ -60,9 +62,10 @@ ui = fluidPage(theme = shinytheme("superhero"),
                  mainPanel(
                      h3(textOutput("MontantPerpetuite"))
                         )
-                     )
-                 ),
-    tabPanel("Effective Annual Rate",
+                     )}
+    ),
+    
+    tabPanel("Effective Annual Rate",{
              sidebarLayout(
                  sidebarPanel(
                      h2("Calcul du Taux Annuel Effectif"),
@@ -79,9 +82,10 @@ ui = fluidPage(theme = shinytheme("superhero"),
                  mainPanel(
                      h3(textOutput("EAR"))
                  )
-             )
+             )}
     ),
-    tabPanel("Discounting",
+    
+    tabPanel("Discounting",{
              sidebarLayout(
                  sidebarPanel(
                      h2("Discounting calcul (pour passer de Future Value à Present Value)"),
@@ -101,9 +105,10 @@ ui = fluidPage(theme = shinytheme("superhero"),
                  mainPanel(
                      h3(textOutput("Discounting"))
                  )
-             )
+             )}
     ),
-    tabPanel("Capitalisation",
+    
+    tabPanel("Capitalisation",{
              sidebarLayout(
                  sidebarPanel(
                      h2("Capitalisation calcul (pour passer de Present Value à Future Value)"),
@@ -123,7 +128,57 @@ ui = fluidPage(theme = shinytheme("superhero"),
                  mainPanel(
                      h3(textOutput("Capitalisation"))
                  )
-             )
+             )}
+    ),
+    
+    tabPanel("IRR",{
+             sidebarLayout(
+                 sidebarPanel(
+                     h2("Calcul de l'IRR"),
+                     br(),
+                     img(src = "IRR.png", height = 70, width = 250, align = "center"),
+                     br(),
+    textInput('vec1', 'Entrer les cashflows (en séparant par une virgule)', "0,1,2,3"),
+    numericInput(inputId = "InvestIRR",
+                 label = "Valeur de l'investissement de départ (ne pas mettre de -) :",
+                 value = 0),
+    textInput('vec2', 'Entrer les périodes (en séparant par une virgule)', "1,2,3,4")
+                 ),
+    mainPanel(
+        h3(verbatimTextOutput("IRR1")),
+        h5("Dans la case 'Entrer les périodes', il faut indiquer 
+           1,2,3,4 si vous avez 4 périodes. Dans le cas où il y aurait
+           2 cashflows sur une même période, par exemple en année 1,
+           il faut alors indiquer 1,1,2,3,4 pour les périodes.", align = "center"),
+    )
+    
+             )}
+    ),
+    
+    tabPanel("NPV",{
+             sidebarLayout(
+                 sidebarPanel(
+                     h2("Calcul de la NPV"),
+                     br(),
+                     img(src = "NPV.png", height = 70, width = 250, align = "center"),
+                     br(),
+                     textInput('vec3', 'Entrer les cashflows (en séparant par une virgule)', "0,1,2,3"),
+                     numericInput(inputId = "InvestNPV",
+                                  label = "Valeur de l'investissement de départ (ne pas mettre de -) :",
+                                  value = 0),
+                     textInput('vec4', 'Entrer les périodes (en séparant par une virgule)', "1,2,3,4"),
+                     numericInput(inputId = "TxNPV",
+                                  label = "Taux (ex : si 1% entrer 1 et non 0,01) :",
+                                  value = 0)
+                 ),
+                 mainPanel(
+                     h3(verbatimTextOutput("NPV1")),
+                     h5("Dans la case 'Entrer les périodes', il faut indiquer 
+           1,2,3,4 si vous avez 4 périodes. Dans le cas où il y aurait
+           2 cashflows sur une même période, par exemple en année 1,
+           il faut alors indiquer 1,1,2,3,4 pour les périodes.", align = "center"),
+                 )
+             )}
     )
     )
 )
@@ -187,6 +242,22 @@ TxCapitalisation2 <- input$TxCapitalisation / 100
 results5 <- input$PresentValue1 * (1+TxCapitalisation2)^input$capitalisationPeriods
 results5 <- format(results5, scientific = FALSE)
 paste("La Future Value vaut ", results5)
+    })
+    
+    output$IRR1 <- renderText({
+vec1x <- as.numeric(unlist(strsplit(input$vec1,",")))
+vec2x <- as.numeric(unlist(strsplit(input$vec2,",")))
+results6 <- IRR(input$InvestIRR, vec1x, vec2x)
+results6 <- results6 * 100
+paste("L'IRR vaut ", results6, "%")
+    })
+    
+    output$NPV1 <- renderText({
+TxNPV1 <- input$TxNPV / 100
+vec3x <- as.numeric(unlist(strsplit(input$vec3,",")))
+vec4x <- as.numeric(unlist(strsplit(input$vec4,",")))
+results7 <- NPV(input$InvestNPV, vec3x, vec4x, TxNPV1)
+paste("La NPV vaut ", results7)
     })
 }
 
